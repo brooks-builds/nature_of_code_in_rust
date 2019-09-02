f986fc711d1fb40021c619724e988f32bc8ca0d0
Brooks Patton <brooks@brookzerker.com>
2019-09-02 08:44:55 -0600

    random distribution is being displayed!

--- introduction/i_2_random_number_distribution/src/main.rs
+++ introduction/i_2_random_number_distribution/src/main.rs
@@ -1,28 +1,5 @@
-use ggez::{ContextBuilder, Context, event, GameResult, graphics};
-use ggez::event::{EventHandler};
-
-struct Game {
-
-}
-
-impl Game {
-    pub fn new(_context: &mut Context) -> Game {
-        // store a vector (20 length) with 0.0 in each
-        // [1.0, 1.0, 2.0]
-        Game {}
-    }
-}
-
-impl EventHandler for Game {
-    fn update(&mut self, _context: &mut Context) -> GameResult<()> {
-        Ok(())
-    }
-
-    fn draw(&mut self, context: &mut Context) -> GameResult<()> {
-        // loop through the stored vector and draw a rectangle for each with height being the value
-        graphics::present(context)
-    }
-}
+use i_2_random_number_distribution::*;
+use ggez::{ContextBuilder, event};
 
 fn main() {
     let (mut context, mut event_loop) = ContextBuilder::new("random_distribution", "Brookzerker")
