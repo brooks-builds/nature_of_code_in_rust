4e8eba9369d4dcc931b8ce8e9fc6b045e3219320
Brooks Patton <brooks@brookzerker.com>
2019-11-29 10:57:04 -0700

    we have a spinning baton, capped rotation

--- chapter_3/exercise_3_1/src/lib.rs
+++ chapter_3/exercise_3_1/src/lib.rs
@@ -1,24 +1,98 @@
-use bbggez::ggez::{event::EventHandler, graphics, Context, GameResult};
+use bbggez::ggez::{event::EventHandler, graphics, nalgebra::Point2, timer, Context, GameResult};
+// use specs::{Component, DenseVecStorage, World, WorldExt};
 
 pub struct Game {
 	background_color: graphics::Color,
+	baton_mesh: graphics::Mesh,
+	angle: f32,
+	angular_velocity: f32,
+	angular_acceleration: f32,
+	angular_velocity_max: f32,
 }
 
 impl Game {
-	pub fn new() -> Game {
+	pub fn new(context: &mut Context) -> GameResult<Game> {
 		let background_color = graphics::Color::from_rgb(128, 207, 216);
+		let cap_radius = 15.0;
+		let pole_length = 300.0;
+		let pole_color = graphics::Color::from_rgb(246, 177, 127);
+		let pole_width = 3.0;
+		let cap_color = graphics::Color::from_rgb(178, 87, 122);
+		let baton_mesh = graphics::MeshBuilder::new()
+			.circle(
+				graphics::DrawMode::fill(),
+				Point2::new(0.0 - pole_length / 2.0 - cap_radius, 0.0),
+				cap_radius,
+				0.0001,
+				cap_color,
+			)
+			.line(
+				&[
+					Point2::new(0.0 - pole_length / 2.0, 0.0),
+					Point2::new(pole_length / 2.0, 0.0),
+				],
+				pole_width,
+				pole_color,
+			)?
+			.circle(
+				graphics::DrawMode::fill(),
+				Point2::new(pole_length / 2.0 + cap_radius, 0.0),
+				cap_radius,
+				0.0001,
+				cap_color,
+			)
+			.build(context)?;
+		let angle = 0.0;
+		let angular_velocity = 0.0;
+		let angular_acceleration = 0.001;
+		let angular_velocity_max = 0.02;
 
-		Game { background_color }
+		Ok(Game {
+			background_color,
+			baton_mesh,
+			angle,
+			angular_velocity,
+			angular_acceleration,
+			angular_velocity_max,
+		})
 	}
 }
 
 impl EventHandler for Game {
 	fn update(&mut self, context: &mut Context) -> GameResult<()> {
+		let delta_time = timer::delta(context).as_secs_f32();
+
+		self.angular_velocity += self.angular_acceleration * delta_time;
+		if self.angular_velocity > self.angular_velocity_max {
+			self.angular_velocity = self.angular_velocity_max;
+		}
+		self.angle += self.angular_velocity;
 		Ok(())
 	}
 
 	fn draw(&mut self, context: &mut Context) -> GameResult<()> {
 		graphics::clear(context, self.background_color);
+
+		let (width, height) = graphics::drawable_size(context);
+
+		graphics::draw(
+			context,
+			&self.baton_mesh,
+			graphics::DrawParam::new()
+				.dest(Point2::new(width / 2.0, height / 2.0))
+				.rotation(self.angle),
+		)?;
+
+		let angular_velocity_text =
+			graphics::Text::new(format!("angular velocity: {}", self.angular_velocity));
+
+		graphics::draw(
+			context,
+			&angular_velocity_text,
+			graphics::DrawParam::new()
+				.dest(Point2::new(5.0, 5.0))
+				.color(graphics::BLACK),
+		)?;
 
 		graphics::present(context)
 	}
--- chapter_3/exercise_3_1/src/main.rs
+++ chapter_3/exercise_3_1/src/main.rs
@@ -1,8 +1,21 @@
-use bbggez::run::run;
+use bbggez::ggez::{event, ContextBuilder};
 use exercise_3_1::Game;
 
 fn main() {
-    let mut game = Game::new();
+    // Make a Context.
+    let (mut context, mut event_loop) =
+        ContextBuilder::new("Angular motion using rotation", "Brookzerker")
+            .build()
+            .expect("aieee, could not create ggez context!");
 
-    run(&mut game, "Rotate Baton-Like Object", "Brookzerker");
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let mut my_game = Game::new(&mut context).unwrap();
+
+    // Run!
+    match event::run(&mut context, &mut event_loop, &mut my_game) {
+        Ok(_) => println!("Exited cleanly."),
+        Err(e) => println!("Error occured: {}", e),
+    }
 }
