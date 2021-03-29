ce4da8570bf54534f46bb1aaacdc6eea59d152c9
Brooks Patton <brooks@brookzerker.com>
2021-03-28 18:11:35 -0600

    updated template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,14 +1,17 @@
 use eyre::Result;
-use ggez::conf::WindowMode;
+use ggez::conf::{WindowMode, WindowSetup};
 use ggez::{event, ContextBuilder};
 use template::MainState;
 
+const TITLE: &str = "Nature of Code in Rust: ";
+
 fn main() -> Result<()> {
     let window_mode = WindowMode::default().dimensions(1920.0, 1080.0);
-    let (mut context, mut events_loop) =
-        ContextBuilder::new("Nature of Code in Rust: ", "Brookzerker")
-            .window_mode(window_mode)
-            .build()?;
+    let window_setup = WindowSetup::default().title(TITLE);
+    let (mut context, mut events_loop) = ContextBuilder::new(TITLE, "Brookzerker")
+        .window_mode(window_mode)
+        .window_setup(window_setup)
+        .build()?;
     let mut main_state = MainState::new(&mut context)?;
     event::run(&mut context, &mut events_loop, &mut main_state)?;
     Ok(())
