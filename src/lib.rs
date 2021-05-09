1e7acb24ea43b558b37a1b8a0b494ff74ae9c3cc
Brooks Patton <brooks@brookzerker.com>
2021-05-09 16:30:34 -0600

    example_i_6

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,47 @@
-mod walker;
-
 use ggez::event::EventHandler;
-use ggez::{graphics, Context, GameResult};
-use walker::Walker;
+use ggez::graphics::{self, Color, DrawMode, DrawParam, MeshBuilder};
+use ggez::{Context, GameResult};
+use noise::{NoiseFn, Perlin};
+use utilities::map;
 
-pub struct MainState {
-    walker: Walker,
-}
+mod utilities;
+
+pub struct MainState {}
 
 impl MainState {
     pub fn new(context: &mut Context) -> GameResult<Self> {
         let (width, height) = graphics::drawable_size(context);
-        let walker = Walker::new(width / 2.0, height / 2.0, context)?;
-        Ok(Self { walker })
+        let mut mesh_builder = MeshBuilder::new();
+
+        let noise = Perlin::new();
+        let noise_incrementer = 0.01;
+
+        let mut xoff = 10_000.0;
+        for x in 0..width as u32 {
+            let mut yoff = 0.0;
+            for y in 0..height as u32 {
+                let brightness = map(noise.get([xoff, yoff]) as f32, -1.0, 1.0, 0.0, 1.0);
+                let color = Color::new(brightness, brightness, brightness, 1.0);
+                mesh_builder.circle(DrawMode::fill(), [x as f32, y as f32], 0.5, 0.1, color);
+                yoff += noise_incrementer;
+            }
+            xoff += noise_incrementer;
+        }
+
+        let mesh = mesh_builder.build(context)?;
+
+        graphics::draw(context, &mesh, DrawParam::new())?;
+        graphics::present(context)?;
+        Ok(Self {})
     }
 }
 
 impl EventHandler for MainState {
-    fn update(&mut self, _context: &mut ggez::Context) -> ggez::GameResult {
-        self.walker.step();
+    fn update(&mut self, _context: &mut Context) -> GameResult {
         Ok(())
     }
 
-    fn draw(&mut self, context: &mut ggez::Context) -> ggez::GameResult {
-        self.walker.display(context)?;
-        graphics::present(context)
+    fn draw(&mut self, _context: &mut Context) -> GameResult {
+        Ok(())
     }
 }
