f3fc75b79bcbc731cc2b590d055612df77bff131
Brooks Patton <brooks@brookzerker.com>
2021-05-09 16:21:23 -0600

    exercise_i_1

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use ggez::event::EventHandler;
 use ggez::graphics::{self, DrawMode, DrawParam, Mesh, MeshBuilder, WHITE};
-use ggez::input::mouse;
 use ggez::mint::Point2;
 use ggez::{Context, GameResult};
 
@@ -12,54 +11,30 @@ pub struct MainState {
 
 impl MainState {
     pub fn new(context: &mut Context) -> GameResult<Self> {
-        let (width, height) = graphics::drawable_size(context);
-        let x = width / 2.0;
-        let y = height / 2.0;
+        let x = 50.0;
+        let y = 50.0;
         let mesh = MeshBuilder::new()
             .circle(DrawMode::fill(), Point2 { x: 0.0, y: 0.0 }, 1.0, 0.1, WHITE)
             .build(context)?;
 
         Ok(Self { x, y, mesh })
     }
-
-    fn move_towards_mouse(&mut self, context: &mut Context) {
-        let mouse_position = mouse::position(context);
-
-        if self.x < mouse_position.x {
-            self.x += 1.0;
-        } else if self.x > mouse_position.x {
-            self.x -= 1.0;
-        }
-
-        if self.y < mouse_position.y {
-            self.y += 1.0;
-        } else if self.y > mouse_position.y {
-            self.y -= 1.0;
-        }
-    }
-
-    fn randomly_move(&mut self) {
-        let random_number = rand::random::<f32>();
-
-        if random_number < 0.25 {
-            self.y -= 1.0;
-        } else if random_number < 0.5 {
-            self.y += 1.0;
-        } else if random_number < 0.75 {
-            self.x += 1.0;
-        } else {
-            self.x -= 1.0;
-        }
-    }
 }
 
 impl EventHandler for MainState {
-    fn update(&mut self, context: &mut Context) -> GameResult {
-        if rand::random::<f32>() < 0.5 {
-            self.move_towards_mouse(context);
+    fn update(&mut self, _context: &mut Context) -> GameResult {
+        let random_number = rand::random::<f32>();
+
+        if random_number < 0.2 {
+            self.y -= 1.0;
+        } else if random_number < 0.4 {
+            self.x -= 1.0;
+        } else if random_number < 0.7 {
+            self.x += 1.0;
         } else {
-            self.randomly_move();
+            self.y += 1.0;
         }
+
         Ok(())
     }
 
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use template::MainState;
 fn main() -> GameResult {
     let window_mode = WindowMode::default().dimensions(1920.0, 1080.0);
     let (mut context, mut events_loop) =
-        ContextBuilder::new("Nature of Code in Rust: Exercise I.3", "Brookzerker")
+        ContextBuilder::new("Nature of Code in Rust: Exercise I.1", "Brookzerker")
             .window_mode(window_mode)
             .build()?;
     let mut main_state = MainState::new(&mut context)?;
