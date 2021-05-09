3a92431e6eb6068180fbf93fa8a12e030dc48e80
Brooks Patton <brooks@brookzerker.com>
2021-05-09 16:29:35 -0600

    exercise_1_4

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,29 @@
 use ggez::event::EventHandler;
 use ggez::graphics::{self, Color, BLACK};
 use ggez::{Context, GameResult};
-use mover::Mover;
 
 mod mover;
 mod utilities;
 
 pub struct MainState {
     background_color: Color,
-    mover: Mover,
 }
 
 impl MainState {
-    pub fn new(context: &mut Context) -> GameResult<Self> {
+    pub fn new(_context: &mut Context) -> GameResult<Self> {
         let background_color = BLACK;
-        let (width, height) = graphics::drawable_size(context);
-        let mover = Mover::new(width / 2.0, height / 2.0, context)?;
 
-        Ok(Self {
-            background_color,
-            mover,
-        })
+        Ok(Self { background_color })
     }
 }
 
 impl EventHandler for MainState {
-    fn update(&mut self, context: &mut Context) -> GameResult {
-        self.mover.handle_input(context);
-        self.mover.update();
-        self.mover.check_edges(context);
+    fn update(&mut self, _context: &mut Context) -> GameResult {
         Ok(())
     }
 
     fn draw(&mut self, context: &mut Context) -> GameResult {
         graphics::clear(context, self.background_color);
-        self.mover.display(context)?;
         graphics::present(context)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use eyre::Result;
 use ggez::conf::{WindowMode, WindowSetup};
 use ggez::{event, ContextBuilder};
-use nature_of_code_in_rust::MainState;
+use template::MainState;
 
-const TITLE: &str = "Nature of Code in Rust";
+const TITLE: &str = "Nature of Code in Rust: ";
 
 fn main() -> Result<()> {
     let window_mode = WindowMode::default().dimensions(1920.0, 1080.0);
