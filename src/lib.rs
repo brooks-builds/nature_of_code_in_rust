4421b11d52804b45f0956ba410d037222dd9c489
Brooks Patton <brooks@brookzerker.com>
2021-05-09 16:31:30 -0600

    example_i_4

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,41 @@
-use eyre::Result;
 use ggez::event::EventHandler;
-use ggez::graphics::{self, Color, BLACK};
+use ggez::graphics::{self, Color, DrawMode, DrawParam, Mesh, MeshBuilder};
+use ggez::mint::Point2;
 use ggez::{timer, Context, GameResult};
-use noise::Perlin;
-use walker::Walker;
-
-mod utilities;
-mod walker;
+use rand::prelude::ThreadRng;
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
 
 pub struct MainState {
-    background_color: Color,
-    walker: Walker,
-    noise: Perlin,
+    y: f32,
+    x: f32,
+    mesh: Mesh,
+    rng: ThreadRng,
+    random: Normal<f32>,
 }
 
 impl MainState {
-    pub fn new(context: &mut Context) -> Result<Self> {
-        let background_color = BLACK;
-        let walker = Walker::new(context)?;
-        let noise = Perlin::new();
-
+    pub fn new(context: &mut Context) -> GameResult<Self> {
+        let (width, height) = graphics::drawable_size(context);
+        let y = height / 2.0;
+        let x = width / 2.0;
+        let mesh = MeshBuilder::new()
+            .circle(
+                DrawMode::fill(),
+                Point2 { x: 0.0, y: 0.0 },
+                10.0,
+                0.1,
+                Color::new(1.0, 1.0, 1.0, 0.01),
+            )
+            .build(context)?;
+        let rng = thread_rng();
+        let random = Normal::new(width / 2.0, 100.0).unwrap();
         Ok(Self {
-            background_color,
-            walker,
-            noise,
+            y,
+            x,
+            mesh,
+            rng,
+            random,
         })
     }
 }
@@ -31,15 +43,14 @@ impl MainState {
 impl EventHandler for MainState {
     fn update(&mut self, context: &mut Context) -> GameResult {
         while timer::check_update_time(context, 60) {
-            let (screen_width, screen_height) = graphics::drawable_size(context);
-            self.walker.step(&self.noise, screen_width, screen_height);
+            self.x = self.random.sample(&mut self.rng);
         }
+
         Ok(())
     }
 
     fn draw(&mut self, context: &mut Context) -> GameResult {
-        graphics::clear(context, self.background_color);
-        self.walker.draw(context).unwrap();
+        graphics::draw(context, &self.mesh, DrawParam::new().dest([self.x, self.y]))?;
         graphics::present(context)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use example_i_5::MainState;
-use eyre::Result;
+use example_i_4::MainState;
 use ggez::conf::WindowMode;
-use ggez::{event, ContextBuilder};
+use ggez::{event, ContextBuilder, GameResult};
 
-fn main() -> Result<()> {
+fn main() -> GameResult {
     let window_mode = WindowMode::default().dimensions(1920.0, 1080.0);
     let (mut context, mut events_loop) = ContextBuilder::new(
-        "Nature of Code in Rust: Example I.5 - Perlin Noise Walker",
+        "Nature of Code in Rust: Example I.4 - Gaussian Distribution",
         "Brookzerker",
     )
     .window_mode(window_mode)
     .build()?;
     let mut main_state = MainState::new(&mut context)?;
-    event::run(&mut context, &mut events_loop, &mut main_state)?;
-    Ok(())
+    event::run(&mut context, &mut events_loop, &mut main_state)
 }
