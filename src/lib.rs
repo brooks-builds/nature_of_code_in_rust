b4d5464df6281c8bee82d9e840f404cd8fba3db9
Brooks Patton <brooks@brookzerker.com>
2021-05-09 16:31:58 -0600

    example_i_2

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,85 @@
 use ggez::event::EventHandler;
-use ggez::graphics::{self, Color, DrawMode, DrawParam, Mesh, MeshBuilder};
-use ggez::mint::Point2;
-use ggez::{timer, Context, GameResult};
+use ggez::graphics::{self, Color, DrawMode, DrawParam, Mesh, MeshBuilder, Rect, BLACK, WHITE};
+use ggez::{Context, GameResult};
 use rand::prelude::ThreadRng;
-use rand::thread_rng;
-use rand_distr::{Distribution, Normal};
+use rand::{thread_rng, Rng};
+
+const RECTANGLE_WIDTH: f32 = 96.0;
 
 pub struct MainState {
-    y: f32,
-    x: f32,
-    mesh: Mesh,
+    background_color: Color,
+    heights: Vec<f32>,
+    meshes: Vec<Mesh>,
+    colors: Vec<Color>,
     rng: ThreadRng,
-    random: Normal<f32>,
 }
 
 impl MainState {
     pub fn new(context: &mut Context) -> GameResult<Self> {
-        let (width, height) = graphics::drawable_size(context);
-        let y = height / 2.0;
-        let x = width / 2.0;
-        let mesh = MeshBuilder::new()
-            .circle(
-                DrawMode::fill(),
-                Point2 { x: 0.0, y: 0.0 },
-                10.0,
-                0.1,
-                Color::new(1.0, 1.0, 1.0, 0.01),
-            )
-            .build(context)?;
-        let rng = thread_rng();
-        let random = Normal::new(width / 2.0, 100.0).unwrap();
+        let mut rng = thread_rng();
+        let background_color = BLACK;
+        let heights = vec![0.0; 20];
+        let meshes = vec![
+            MeshBuilder::new()
+                .rectangle(
+                    DrawMode::fill(),
+                    Rect::new(0.0, 0.0, RECTANGLE_WIDTH, 10.0),
+                    WHITE
+                )
+                .build(context)?;
+            20
+        ];
+        let mut colors = vec![];
+
+        for _ in 0..20 {
+            let random_color = Color::from_rgb(
+                rng.gen_range(200..255),
+                rng.gen_range(200..255),
+                rng.gen_range(200..255),
+            );
+            colors.push(random_color);
+        }
+
         Ok(Self {
-            y,
-            x,
-            mesh,
+            background_color,
+            heights,
+            meshes,
+            colors,
             rng,
-            random,
         })
     }
 }
 
 impl EventHandler for MainState {
     fn update(&mut self, context: &mut Context) -> GameResult {
-        while timer::check_update_time(context, 60) {
-            self.x = self.random.sample(&mut self.rng);
+        let (_screen_width, screen_height) = graphics::drawable_size(context);
+
+        let random_index = self.rng.gen_range(0..self.heights.len());
+        self.heights[random_index] += 1.0;
+
+        for index in 0..self.meshes.len() {
+            let height = self.heights[index];
+            let start_x = index as f32 * RECTANGLE_WIDTH;
+            let start_y = screen_height - height;
+            let rect = Rect::new(start_x, start_y, RECTANGLE_WIDTH, height);
+            let color = self.colors[index];
+            let mesh = MeshBuilder::new()
+                .rectangle(DrawMode::fill(), rect, color)
+                .rectangle(DrawMode::stroke(2.0), rect, BLACK)
+                .build(context)?;
+            self.meshes[index] = mesh;
         }
 
         Ok(())
     }
 
     fn draw(&mut self, context: &mut Context) -> GameResult {
-        graphics::draw(context, &self.mesh, DrawParam::new().dest([self.x, self.y]))?;
+        graphics::clear(context, self.background_color);
+
+        for mesh in &self.meshes {
+            graphics::draw(context, mesh, DrawParam::new())?;
+        }
+
         graphics::present(context)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use example_i_4::MainState;
 use ggez::conf::WindowMode;
 use ggez::{event, ContextBuilder, GameResult};
+use template::MainState;
 
 fn main() -> GameResult {
     let window_mode = WindowMode::default().dimensions(1920.0, 1080.0);
-    let (mut context, mut events_loop) = ContextBuilder::new(
-        "Nature of Code in Rust: Example I.4 - Gaussian Distribution",
-        "Brookzerker",
-    )
-    .window_mode(window_mode)
-    .build()?;
+    let (mut context, mut events_loop) =
+        ContextBuilder::new("Nature of Code in Rust: I.2", "Brookzerker")
+            .window_mode(window_mode)
+            .build()?;
     let mut main_state = MainState::new(&mut context)?;
     event::run(&mut context, &mut events_loop, &mut main_state)
 }
